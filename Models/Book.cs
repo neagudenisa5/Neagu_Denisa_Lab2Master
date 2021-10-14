ceef6fa0e382280bfcf6374a521c48f717a81810
Denisa Neagu <neagudenisa140@gmail.com>
2021-10-14 15:32:07 +0300

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neagu_Denisa_Lab2.Models
+{
+    public class Book
+    {
+    }
+}
