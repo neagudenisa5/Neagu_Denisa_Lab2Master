5c9178724ede22a434496916e29ccef770288777
Denisa Neagu <neagudenisa140@gmail.com>
2021-10-14 16:08:56 +0300

    .

--- Models/Book.cs
+++ Models/Book.cs
@@ -7,5 +7,10 @@ namespace Neagu_Denisa_Lab2.Models
 {
     public class Book
     {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public ICollection<Order> Orders { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Neagu_Denisa_Lab2.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Neagu_Denisa_Lab2
 {
@@ -24,6 +26,8 @@ namespace Neagu_Denisa_Lab2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<LibraryContext>(options =>options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
