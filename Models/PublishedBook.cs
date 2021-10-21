5ce466bc5a9c0bf6413861a6c06bd119135e8090
Denisa Neagu <neagudenisa140@gmail.com>
2021-10-22 01:01:55 +0300

    lab4

--- Models/PublishedBook.cs
+++ Models/PublishedBook.cs
@@ -5,13 +5,12 @@ using System.Threading.Tasks;
 
 namespace Neagu_Denisa_Lab2.Models
 {
-    public class Order
+    public class PublishedBook
     {
-        public int OrderID { get; set; }
-        public int CustomerID { get; set; }
+        public int PublisherID { get; set; }
         public int BookID { get; set; }
-        public DateTime OrderDate { get; set; }
-        public Customer Customer { get; set; }
+        public Publisher Publisher { get; set; }
         public Book Book { get; set; }
+
     }
 }
